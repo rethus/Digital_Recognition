59637256c82ef2c704a13bd010178121c15a80b3
rethus <875063510@qq.com>
2023-01-10 16:41:57 +0800

    20230110: edit predict

--- demo/run.py
+++ demo/run.py
@@ -3,9 +3,9 @@ from predict import Predict
 from net import AlexNet, BPNet, LeNet
 
 if __name__ == '__main__':
-    Train(AlexNet(num_classes=10), '../pre_train/LeNet.pth', epoch_num=20)
-    Train(BPNet(), '../pre_train/BPNet.pth', epoch_num=20)
-    Train(LeNet(), '../pre_train/LeNet.pth', epoch_num=20)
+    # Train(AlexNet(num_classes=10), '../pre_train/LeNet.pth', epoch_num=20)
+    # Train(BPNet(), '../pre_train/BPNet.pth', epoch_num=100)
+    # Train(LeNet(), '../pre_train/LeNet.pth', epoch_num=100)
 
     Predict(AlexNet(num_classes=10), '../pre_train/AlexNet.pth')
     Predict(BPNet(), '../pre_train/BPNet.pth')
